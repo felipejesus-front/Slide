b69914aef5b475937726397e6e14c3a90fb7cdf6
Felipe Campos <felipe.campos@al.resilia.com.br>
2022-06-19 19:21:27 -0300

    eventos de touchs adicionados

--- js/slide.js
+++ js/slide.js
@@ -7,7 +7,7 @@ export default class Slide {
 
     moveSlide(distX){
         this.dist.movePosition = distX
-        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;s
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
     }
 
     updatePosition(clientX){
@@ -16,13 +16,21 @@ export default class Slide {
     }
 
     onStart(event){
-        event.preventDefault();
-        this.dist.startX = event.clientX
-        this.wrapper.addEventListener('mousemove', this.onMove);
+        let movetype;
+        if(event.type === 'mousedown') {
+            event.preventDefault();
+            this.dist.startX = event.clientX
+            movetype = 'mousemove'
+        } else {
+            this.dist.startX = event.changedTouches[0].clientX;
+            movetype = 'touchmove'
+        }
+        this.wrapper.addEventListener(movetype, this.onMove);
     }
 
     onMove(event){
-        const finalPosition = this.updatePosition(event.clientX);
+        const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+        const finalPosition = this.updatePosition(pointerPosition);
         this.moveSlide(finalPosition)
     }
 
@@ -33,7 +41,9 @@ export default class Slide {
 
     addSlideEvents(){
         this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('touchstart', this.onStart);
         this.wrapper.addEventListener('mouseup', this.onEnd);
+        this.wrapper.addEventListener('touchend', this.onEnd);
     }
 
     bindEvents(){
