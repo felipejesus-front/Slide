d6b5cfa493abda2159f579bd37a4531e048328b8
Felipe Campos <felipe.campos@al.resilia.com.br>
2022-06-20 12:56:20 -0300

    adicionada a paginação por slide

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ import { SlideNav } from './slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
-slide.addArrow('.prev', '.next')
+slide.addArrow('.prev', '.next');
+slide.addControl();
